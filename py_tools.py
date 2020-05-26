0e902d4270b1c3194baacb35263d90be2f7daecd
Jkutkut <jorgeregonzalez@gmail.com>
2020-05-26 23:22:16 +0200

    removed semicolons

--- py_tools.py
+++ py_tools.py
@@ -1,47 +1,47 @@
 ## Prints the sudoku array
 def printSudoku(arr):
     print(*["  Y "] + [str(i) + " " for i in range(3)] + ["  "] + [str(3+i) + " " for i in range(3)] + ["  "] + [str(6+i) + " " for i in range(3)], sep = "")
-    print(*["X +"] + ["-" for i in range(23)] + ["+"], sep = "");#start
+    print(*["X +"] + ["-" for i in range(23)] + ["+"], sep = "")#start
 
     for i in range(3): #rows
         t = [str(i) + " |"] + arr[i][0:3] + ["|"] + arr[i][3:6] + ["|"] + arr[i][6:9] + ["|"]
         print(*t, sep = " ")
 
-    print(*["  "]+["".join(["+"] + ["-" for i in range(7)]) for j in range(3)] + ["+"], sep = "");#3 by 3 separators
+    print(*["  "]+["".join(["+"] + ["-" for i in range(7)]) for j in range(3)] + ["+"], sep = "")#3 by 3 separators
 
     for i in range(3, 6): #rows
         t = [str(i) + " |"] + arr[i][0:3] + ["|"] + arr[i][3:6] + ["|"] + arr[i][6:9] + ["|"]
         print(*t, sep = " ")
 
-    print(*["  "]+["".join(["+"] + ["-" for i in range(7)]) for j in range(3)] + ["+"], sep = "");#3 by 3 separators
+    print(*["  "]+["".join(["+"] + ["-" for i in range(7)]) for j in range(3)] + ["+"], sep = "")#3 by 3 separators
 
     for i in range(6, 9): #rows
         t = [str(i) + " |"] + arr[i][0:3] + ["|"] + arr[i][3:6] + ["|"] + arr[i][6:9] + ["|"]
         print(*t, sep = " ")
 
-    print(*["  +"] + ["-" for i in range(23)] + ["+"], sep = "");#end
+    print(*["  +"] + ["-" for i in range(23)] + ["+"], sep = "")#end
 
 ## Prints the status of the arguments (errors)
 def pError(**kwargs):
-    print("***ERROR***".center(30));
-    print(kwargs);
-    print("***END ERROR***".center(30));
+    print("***ERROR***".center(30))
+    print(kwargs)
+    print("***END ERROR***".center(30))
 
 ## Checks if it is possible to place "value" on the position (x, y) or data[x][y].
 def isPossibleVal(x, y, value, arr):
     for i in range(9):
         if data[x][i] == value:
-            return False;
+            return False
         if data[i][y] == value:
-            return False;
+            return False
 
-    xIndex = (x // 3) * 3;
-    yIndex = (y // 3) * 3;
+    xIndex = (x // 3) * 3
+    yIndex = (y // 3) * 3
     for i in range(3):
         for j in range(3):
             if data[xIndex + i][yIndex + j] == value:
-                return False;
-    return True;
+                return False
+    return True
 
 # Given the sudoku as data and an array to store all the solutions 
 def sudokuSolution(data, solutions):
@@ -75,14 +75,14 @@ def checkSol(arr):
                     suma = suma + arr[k + i][l + j]
                     ele = ele + [arr[k + i][l + j]]
             if(suma != 45):
-                # pError(zone="3b3", i=i, j=j, k=k, l=l, ele=ele);
+                # pError(zone="3b3", i=i, j=j, k=k, l=l, ele=ele)
                 return False
     for l in range(9):#lines:
         solC = 0
         for i in range(9):
             solC = solC + arr[i][l]
         if sum(arr[l]) != 45 or solC != 45:
-            # pError(zone="lines", i=i, l=l);
+            # pError(zone="lines", i=i, l=l)
             return False
     return True
 
